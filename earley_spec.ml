306d907dadad2088f0da79e69ef7ed805ab4ce60
Tom Ridge <tom.j.ridge@googlemail.com>
2018-02-12 16:10:02 +0000

    compiles

--- earley_spec.ml
+++ earley_spec.ml
@@ -22,53 +22,64 @@ type raw_item_set
 (* complete and blocked maps are from k,S *)
 type 'a map_int_symbol
 
+type int_set
+
 type state = {
   todo_done: item_set;
   todo: item list;
   blocked: raw_item_set map_int_symbol;
-  complete: int set map_int_symbol  (* map k,S -> j set *)
+  complete: int_set map_int_symbol  (* map k,S -> j set *)
 }
   
 
 let step 
-    ~item_set_ops ~raw_set_ops ~raw_set_to_list ~map_ops 
-    ~add_item ~cut ~expand (s:state) = 
+    ~item_set_ops 
+    ~raw_set_ops 
+    ~raw_set_to_list 
+    ~complete_ops
+    ~blocked_ops 
+    ~int_set_ops
+    ~int_set_to_list
+    ~add_item 
+    ~cut 
+    ~expand 
+    (s:state) 
+  = 
   match s.todo with
   | [] -> None
   | x::xs -> 
     let s = {s with todo=xs } in
     match x with
     | Cut_complete (k,_S,j) -> (
-      (* cut complete item kSj with items blocked on k,S *)
-      map_ops.map_find (k,_S) s.blocked |> function
-      | None -> Some s
-      | Some bs -> 
-        (* process each of these blocked items against the complete item *)
-        bs 
-        |> raw_set_to_list (* FIXME possible inefficiency *)
-        |> Tjr_list.with_each_elt
-          ~init_state:s
-          ~step:(fun ~state blocked_item ->
-              let new_item = cut blocked_item j in
-              add_item new_item s)
-        |> fun x -> Some x)
-    | Cut_blocked blocked_item -> (
-        let (k,_S) = (blocked_item.k, List.hd blocked_item.bs) in
-        map_ops.map_find (k,_S) s.complete |> function
+        (* cut complete item kSj with items blocked on k,S *)
+        blocked_ops.map_find (k,_S) s.blocked |> function
         | None -> Some s
-        | Some cs ->
-          cs 
-          |> raw_set_to_list
+        | Some bs -> 
+          (* process each of these blocked items against the complete item *)
+          bs 
+          |> raw_set_to_list (* FIXME possible inefficiency *)
           |> Tjr_list.with_each_elt
             ~init_state:s
-            ~step:(fun ~state j ->
+            ~step:(fun ~state blocked_item ->
                 let new_item = cut blocked_item j in
-                add_item new_item s))
+                add_item new_item s)
+          |> fun x -> Some x)
+    | Cut_blocked blocked_item -> (
+        let (k,_S) = (blocked_item.k, List.hd blocked_item.bs) in
+        complete_ops.map_find (k,_S) s.complete |> fun js ->
+        js 
+        |> int_set_to_list
+        |> Tjr_list.with_each_elt
+          ~init_state:s
+          ~step:(fun ~state j ->
+              let new_item = cut blocked_item j in
+              add_item new_item s)
+        |> fun s -> Some s)
     | Expand (i,_S) -> (expand (i,_S) s)  
     | Raw itm ->
       match itm.bs with
       | [] -> 
-        add_item (Cut_complete (itm.i,itm.nt,itm.k)) s
+        Some (add_item (Cut_complete (itm.i,NT itm.nt,itm.k)) s)
       | _S::bs -> 
         s |> add_item (Expand (itm.k,_S)) |> add_item (Cut_blocked itm) 
         |> fun s -> Some s
