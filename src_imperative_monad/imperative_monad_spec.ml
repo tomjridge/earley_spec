8baa6928990e41b464e8483bdf0b6fb012a7d324
Tom Ridge <tom.j.ridge@googlemail.com>
2018-05-25 15:44:40 +0100

    +

--- src_imperative_monad/imperative_monad_spec.ml
+++ src_imperative_monad/imperative_monad_spec.ml
@@ -4,32 +4,14 @@ module type M_ = sig
   val return: 'a -> 'a m
 end
 
-module type S_ = sig  
-
-
-  (* FIXME don't have to be so abstract for specification - use
-     concrete implementations of these *)
-  type i_t = int  
-  type k_t = int
-  type j_t = int
-
+module type S_ = sig
   type nt
   type tm
   type sym
   val sym_case: nt:(nt -> 'a) -> tm:(tm -> 'a) -> sym -> 'a
-  val _NT: nt -> sym
 
-  type nt_item  
-
-  val dot_nt: nt_item -> nt
-  val dot_i: nt_item -> i_t
-  val dot_k: nt_item -> k_t
-  val dot_bs: nt_item -> sym list
-
-  val cut: nt_item -> j_t -> nt_item
-
-  type nt_item_set
-  val elements : nt_item_set -> nt_item list
+  type nt_item = { nt:nt; (* as *) i:int; k:int; bs:sym list }
+  val cut: nt_item -> int -> nt_item
 end
 
 module Make(M:M_)(S:S_) = struct
@@ -43,50 +25,50 @@ module Make(M:M_)(S:S_) = struct
     add_items: nt_item list -> unit m;
   }
 
-  let is_complete itm = (itm|>dot_bs = [])
+  let is_complete itm = (itm.bs = [])
 
   (* We construct sets of Earley items indexed by nat; this step
      function takes a set S_n and produces the set S_{n+1} *)
-  let step ~at_ops ~expand_nt ~expand_tm ~input ~input_length ~get_blocked_items = 
+  let earley ~at_ops ~expand_nt ~expand_tm ~input ~get_blocked_items = 
     let { get_items; add_items } = at_ops in
-    get_items () >>= fun previous_itms ->
-    let rec loop itms = 
-      match itms with 
-      | [] -> return ()
-      | itm::rest ->
-        match itm|>dot_bs with
-        | [] -> 
-          (* item is complete *)
-          let (k',_Y,k) = (itm|>dot_i, itm|>dot_nt, itm|>dot_k) in
-          (* NOTE in order to be clear about the stages, we need to
-             get blocked items from the previous stage; this ensures
-             that the processing of each item at this stage does not
-             interfere with each other - the loop could be
-             "parallel" *)
-          get_blocked_items (k',_Y) previous_itms |> fun bitms ->
-          bitms |> List.map (fun bitm -> cut bitm k) |> fun itms ->
-          add_items itms >>= fun _ ->
-          loop rest
-        | _S::bs ->
-          (* item is not complete; deal with symbol S *)
-          let bitm = itm in
-          let k = bitm|>dot_k in
-          _S |> sym_case
-            ~nt:(fun _Y -> 
-                expand_nt ~k ~nt:_Y |> fun itms ->
-                add_items itms >>= fun _ ->
-                loop rest)
-            ~tm:(fun _T ->
-                expand_tm ~k ~tm:_T ~input ~input_length |> fun js ->
-                js |> List.map (fun j -> cut bitm j) |> fun itms ->
-                add_items itms >>= fun _ ->
-                loop rest)
+    let rec step_set_of_states () = 
+      get_items () >>= fun previous_itms ->
+      let rec loop itms = 
+        match itms with 
+        | [] -> return ()
+        | itm::rest ->
+          match itm.bs with
+          | [] -> 
+            (* item is complete *)
+            let (k',_Y,k) = (itm.i, itm.nt, itm.k) in
+            (* NOTE in order to be clear about the stages, we need to
+               get blocked items from the previous stage; this ensures
+               that the processing of each item at this stage does not
+               interfere with each other - the loop over the items could
+               be "parallel" *)
+            get_blocked_items (k',_Y) previous_itms |> fun bitms ->
+            bitms |> List.map (fun bitm -> cut bitm k) |> fun itms ->
+            add_items itms >>= fun _ ->
+            loop rest
+          | _S::bs ->
+            (* item is not complete; deal with symbol S *)
+            let bitm = itm in
+            let k = bitm.k in
+            _S |> sym_case
+              ~nt:(fun _Y -> 
+                  add_items (expand_nt ~k ~nt:_Y) >>= fun _ ->
+                  loop rest)
+              ~tm:(fun _T ->
+                  expand_tm ~k ~tm:_T ~input |> fun js ->
+                  js |> List.map (fun j -> cut bitm j) |> fun itms ->
+                  add_items itms >>= fun _ ->
+                  loop rest)
+      in
+      loop previous_itms
     in
-    loop previous_itms
+    (* repeatedly apply *)
 
   (* NOTE the mathematical spec of the above as sets indexed by nat would look better *)
-                
-                
-          
-
+      
+             
 end
